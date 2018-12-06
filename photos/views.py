a4bb7546a3ed6d9d0a5451281c4920f779d234ec
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-12-06 15:55:29 +0300

    add dynamic urls for datetime to display todays photos and past photos

---
+++ photos/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime as dt
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to Gallery Website')
+
+def photos_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1>{date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def photos_of_day(request):
+    date = dt.datetime.today()
+
+    # function to convert date object to find exact day
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Photos for {date} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def past_days_photos(request,past_date):
+    # Converts data from the string url
+ date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
