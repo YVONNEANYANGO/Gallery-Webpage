2880373a69ed202020e2a5a09a17d39797fb3650
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2018-12-13 11:20:38 +0300

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn GALLERY-WEBPAGE.wsgi
--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'personal_gallery.urls'
@@ -82,6 +83,8 @@ DATABASES = {
         'NAME': 'photos2',
         'USER':'moringaschool',
         'PASSWORD': 'valleryyt'
+        # 'HOST': config('DB_HOST'),
+        # 'PORT': '',
     }
 }
 
